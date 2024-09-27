e6da7426ddb54695e583f5e5883bdf549c1119ef
pray-lee <praycis@gmail.com>
2024-09-27 20:39:07 +0800

    fucking react

--- src/DeferredValue.jsx
+++ src/DeferredValue.jsx
@@ -1,9 +1,9 @@
 import React, { useDeferredValue, memo, useState } from "react";
 
 export default function DeferredValue() {
-  console.log("===============");
   const [value, setValue] = useState("");
   const deferredValue = useDeferredValue(value);
+  console.log(value.length, "current");
   return (
     <div>
       <input value={value} onChange={(e) => setValue(e.target.value)} />
@@ -13,8 +13,8 @@ export default function DeferredValue() {
 }
 
 const List = memo(function List({ value }) {
-  console.log("123123123123");
   let items = [];
+  console.log(value.length);
   for (let i = 0; i < 250; i++) {
     items.push(<SlowItem text={value}></SlowItem>);
   }
@@ -22,9 +22,5 @@ const List = memo(function List({ value }) {
 });
 function SlowItem({ text }) {
   let startTime = performance.now();
-  console.log(4444444);
-  while (performance.now() - startTime < 100) {
-    // 每个 item 暂停 1ms，模拟极其缓慢的代码
-  }
   return <li className="item">Text: {text}</li>;
 }
